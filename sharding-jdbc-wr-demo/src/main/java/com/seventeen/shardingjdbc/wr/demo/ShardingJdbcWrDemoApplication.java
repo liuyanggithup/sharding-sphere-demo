4b602a3510f339ddbd5145c0e1a180ed25d837be
xia_xun <1529068093@qq.com>
2019-04-08 19:08:49 +0800

    读写分离，集成durid

--- sharding-jdbc-wr-demo/src/main/java/com/seventeen/shardingjdbc/wr/demo/ShardingJdbcWrDemoApplication.java
+++ sharding-jdbc-wr-demo/src/main/java/com/seventeen/shardingjdbc/wr/demo/ShardingJdbcWrDemoApplication.java
@@ -1,8 +1,14 @@
 package com.seventeen.shardingjdbc.wr.demo;
 
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
 /**
  * @author: xia_xun
  * @Date: 2019/4/8
@@ -16,4 +22,23 @@ public class ShardingJdbcWrDemoApplication {
         SpringApplication.run(ShardingJdbcWrDemoApplication.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean getFilterRegistrationBean(){
+        FilterRegistrationBean filter = new FilterRegistrationBean();
+        filter.setFilter(new WebStatFilter());
+        filter.setName("druidWebStatFilter");
+        filter.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.bmp,*.png,*.css,*.ico,/druid/*");
+        filter.addUrlPatterns("/*");
+        return filter;
+    }
+
+    @Bean
+    public ServletRegistrationBean getServletRegistrationBean(){
+        ServletRegistrationBean servlet = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+        servlet.setName("druidStatViewServlet");
+        servlet.addInitParameter("resetEnable", "false");
+        servlet.addInitParameter("loginUsername","admin");
+        servlet.addInitParameter("loginPassword","123456");
+        return servlet;
+    }
 }
